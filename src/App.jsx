fb7300ae3309f4be170de4779a15696d893ef1e4
Ntandoyabalele Mngomezulu <MNGNTA011@myuct.ac.za>
2025-08-16 14:02:11 +0200

    Grade 11 Feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import './App.css'
+// import Navigation from './components/Navigation'
+// import ImpactDashboard from './pages/ImpactDashboard'
+// import Grade12Feedback2025 from './pages/Grade12Feedback2025'
+// import FeedbackAnalysis from './pages/FeedbackAnalysis'
+
+// function App() {
+//   return (
+//     <Router>
+
+//          https://forms.gle/Gi1gUrA4bNxSxTDo6
+
+//       {/* <div className="min-h-screen bg-gray-50">
+//         <Navigation />
+//         <Routes>
+//           <Route path="/" element={<ImpactDashboard />} />
+//           <Route path="/grade12-2025-feedback" element={<Grade12Feedback2025 />} />
+//           <Route path="/feedback-analysis" element={<FeedbackAnalysis />} />
+//         </Routes>
+//       </div> */}
+//     </Router>
+//   )
+// }
+
+// export default App
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navigation from './components/Navigation'
@@ -8,9 +35,18 @@ import FeedbackAnalysis from './pages/FeedbackAnalysis'
 function App() {
   return (
     <Router>
+      <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-6">
+        <a
+          href="https://forms.gle/Gi1gUrA4bNxSxTDo6"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 underline text-lg font-semibold"
+        >
+          Fill out the Grade 11 Feedback Form
+        </a>
+      </div>
 
-         https://forms.gle/Gi1gUrA4bNxSxTDo6
-
+      {/* If you want to enable routes later, just uncomment this block */}
       {/* <div className="min-h-screen bg-gray-50">
         <Navigation />
         <Routes>
