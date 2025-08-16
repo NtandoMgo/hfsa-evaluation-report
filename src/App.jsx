ba1e4ae70f9757fdce69c0bd3a416316303c5d20
Ntandoyabalele Mngomezulu <MNGNTA011@myuct.ac.za>
2025-08-16 13:47:04 +0200

    Grade 11 Feedback

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,17 @@ import FeedbackAnalysis from './pages/FeedbackAnalysis'
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+
+         https://forms.gle/Gi1gUrA4bNxSxTDo6
+
+      {/* <div className="min-h-screen bg-gray-50">
         <Navigation />
         <Routes>
           <Route path="/" element={<ImpactDashboard />} />
           <Route path="/grade12-2025-feedback" element={<Grade12Feedback2025 />} />
           <Route path="/feedback-analysis" element={<FeedbackAnalysis />} />
         </Routes>
-      </div>
+      </div> */}
     </Router>
   )
 }
